cfcfb4badec2c5af292fe944f2e044d6fc503f01
Sergey <52304443+c29r3@users.noreply.github.com>
2020-09-23 17:56:32 +0300

    Delete update_config.sh

--- update_config.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -s https://raw.githubusercontent.com/c29r3/witnet-utils/master/witnet.toml > /root/witnet/witnet1/config/witnet.toml \
-&& sed -e 's/public_addr = "EXTERNAL_IP_ADDRESS:21337"/public_addr = "'"$(curl -s 2ip.ru)":21337'"/g' /root/witnet/witnet1/config/witnet.toml
