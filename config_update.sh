fa7400ec0df7e65ced3187b9d4ec24a06cfca05d
Sergey <52304443+c29r3@users.noreply.github.com>
2020-10-14 23:33:52 +0300

    Update config_update.sh

--- config_update.sh
+++ config_update.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 curl -s https://raw.githubusercontent.com/c29r3/witnet-utils/master/docker-compose.yml > /root/witnet/docker-compose.yml; \
 curl -s https://raw.githubusercontent.com/c29r3/witnet-utils/master/witnet.toml > /root/witnet/witnet1/config/witnet.toml; \
 sed -i -e 's/public_addr = "EXTERNAL_IP_ADDRESS:21337"/public_addr = "'"$(curl -s 2ip.ru)":21337'"/g' /root/witnet/witnet1/config/witnet.toml; \
